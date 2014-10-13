318a1956ef16290ca6e16c0d6002e4110da9e2be
Ben Fogel <legofneb@gmail.com>
2014-10-13 10:15:50 -0500

    Added comments to batch file that warns user of configuration errors

--- execute.bat
+++ execute.bat
@@ -1,3 +1,7 @@
+:: Runs script from Windows machine, this is required if you have both python2 and python3 installed
+:: You may need modification. This assumes python was installed with 'choco install'
+
 set PYTHONHOME=C:\tools\python\
 
+:: My path to the scraper, you will need to change
 C:\tools\python\python.exe Z:\Documents\Python\NetworkRegistrarScraper\scrape.py
